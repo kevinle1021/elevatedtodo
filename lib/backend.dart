a3da85988281daf6337038a211864fd2fa11adf0
Kevin Le <kl626@cornell.edu>
2023-05-31 02:28:50 -0400

    added firestore functionality

--- lib/backend.dart
+++ lib/backend.dart
@@ -10,3 +10,48 @@ Future<void> initializeFirebase() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 }
+
+final reminderCollection = FirebaseFirestore.instance.collection('reminders');
+
+class ToDoConverter {
+  static Map<String, dynamic> toFirestore(ToDo todo, SetOptions? options) {
+    return {
+      'title': todo.title,
+      'description': todo.description,
+      'completed': todo.completed,
+      'dueDate': todo.dueDate,
+    };
+  }
+
+  static ToDo fromFirestore(DocumentSnapshot<Map<String, dynamic>> snapshot,
+      SnapshotOptions? options) {
+    final data = snapshot.data()!;
+    return ToDo(
+        title: data['title'],
+        description: data['description'],
+        completed: data['completed'],
+        dueDate: data['dueDate'].toDate());
+  }
+}
+
+final reminderConverter = reminderCollection.withConverter<ToDo>(
+  fromFirestore: ToDoConverter.fromFirestore,
+  toFirestore: ToDoConverter.toFirestore,
+);
+
+Stream<List<ToDo>> read() {
+  return reminderConverter
+      .orderBy('dueDate', descending: false)
+      .snapshots()
+      .map((snapshot) {
+    return snapshot.docs.map((doc) => doc.data()).toList();
+  });
+}
+
+Future<void> create(ToDo todo) async {
+  try {
+    await reminderConverter.add(todo);
+  } catch (e) {
+    print(e);
+  }
+}
